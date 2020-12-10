8d2ba101a6d4f035b22d842d5cf92e423496df80
Eduardo Correia <eduardo.correia.ehsc@gmail.com>
2020-12-10 20:14:17 -0300

    Rota de criação de um tópico

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,7 +8,25 @@ app.use(express.json());
 app.get('/topics', async (req, res) => {
     const results = await knex('topics');
 
-    res.json(results);
+    return res.status(200).json(results);
+})
+
+app.post('/topics', async (req, res) => {
+    const {
+        title,
+        subject,
+        course,
+        author,
+        content
+    } = req.body;
+
+    try {
+        await knex('topics').insert({title, subject, course, author, content});
+    } catch (error) {
+        return res.status(404).json({message: error});
+    }
+
+    return res.status(201).json({message: 'Success!'});
 })
 
 app.listen(4281, () => console.log("Listening on port 4281!"));
