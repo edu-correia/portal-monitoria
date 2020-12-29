7ece25e3db305fdfa552cfa92612307af72671d4
Eduardo Correia <eduardo.correia.ehsc@gmail.com>
2020-12-29 17:01:10 -0300

    LandingPage finalizada

--- desktop/src/pages/LandingPage/index.jsx
+++ desktop/src/pages/LandingPage/index.jsx
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
-import './styles.css';
+import api from '../../services/api';
 
 import Header from '../../components/Header';
 import SideBar from '../../components/SideBar';
+import Topic from '../../components/Topic';
+
+import './styles.css';
 
 function LandingPage(){
+    const [topics, setTopics] = useState([]);
+
+    useEffect(() => {
+        api.get('/topics').then(res => setTopics(res.data));
+    }, [])
+
     return (
         <>
             <Header />
             <SideBar active="history"/>
+            <div className="topic-list">
+                {topics.map((topic) => {
+                    return (
+                        <Topic 
+                            key={topic.id}
+                            title={topic.title}
+                            subject={topic.subject}
+                            course={topic.course}
+                            author={topic.author}
+                        />
+                    )
+                })}
+            </div>
         </>
     )
 }
