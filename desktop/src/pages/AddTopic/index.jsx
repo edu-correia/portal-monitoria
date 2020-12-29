e2facdebf0a13cd64788b060ac42147ce0464881
Eduardo Correia <eduardo.correia.ehsc@gmail.com>
2020-12-29 18:08:46 -0300

    Sistema de Adição de Tópicos

--- desktop/src/pages/AddTopic/index.jsx
+++ desktop/src/pages/AddTopic/index.jsx
@@ -8,6 +8,30 @@ import SideBar from '../../components/SideBar';
 import './styles.css';
 
 function AddTopic(){
+    const [title, setTitle] = useState('');
+    const [author, setAuthor] = useState('');
+    const [subject, setSubject] = useState('');
+    const [course, setCourse] = useState('');
+    const [content, setContent] = useState('');
+
+    const handleFormSubmit = () => {
+        const data = {
+            title,
+            author,
+            subject,
+            course,
+            content
+        }
+
+        console.log(data);
+
+        api.post('topics', data).then((response) => {
+            if(response.status === 201) alert('Success!');
+          }, (error) => {
+            console.log(error);
+          });
+    }
+
     return (
         <>
             <Header />
@@ -15,24 +39,31 @@ function AddTopic(){
             <div className="form">
                 <div className="input-group">
                     <label htmlFor="title">Título:</label>
-                    <input type="text" id="title"/>
+                    <input type="text" id="title" onChange={(e) => setTitle(e.target.value)}/>
                 </div>
                 <div className="input-group">
                     <label htmlFor="author">Autor:</label>
-                    <input type="text" id="author"/>
+                    <input type="text" id="author" onChange={(e) => setAuthor(e.target.value)}/>
                 </div>
                 <div className="input-group">
                     <label htmlFor="course">Curso:</label>
-                    <input type="text" id="course"/>
+                    <select value={course} onChange={(e) => setCourse(e.target.value)}id="course">
+                        <option value="" disabled hidden>Selecione uma opção</option>
+                        <option value="1MEC">1º Mecânica</option>
+                        <option value="2MEC">2º Mecânica</option>
+                        <option value="3MEC">3º Mecânica</option>
+                        <option value="1INF">1º Informática</option>
+                    </select>
                 </div>
                 <div className="input-group">
                     <label htmlFor="subject">Matéria:</label>
-                    <input type="text" id="subject"/>
+                    <input type="text" id="subject" onChange={(e) => setSubject(e.target.value)}/>
                 </div>
                 <div className="input-group">
                     <label htmlFor="text">Conteúdo:</label>
-                    <textarea id="text"></textarea>
+                    <textarea id="text" onChange={(e) => setContent(e.target.value)}></textarea>
                 </div>
+                <button onClick={handleFormSubmit}>Criar</button>
             </div>
         </>
     )
