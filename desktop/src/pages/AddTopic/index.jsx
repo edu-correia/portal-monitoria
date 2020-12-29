a62a8795f79f9e35a49fd084bdd0fdde43ddfcd1
Eduardo Correia <eduardo.correia.ehsc@gmail.com>
2020-12-29 19:12:02 -0300

    Adicionado o modo Preview

--- desktop/src/pages/AddTopic/index.jsx
+++ desktop/src/pages/AddTopic/index.jsx
@@ -1,9 +1,11 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 
 import api from '../../services/api';
 
 import Header from '../../components/Header';
 import SideBar from '../../components/SideBar';
+
+import marked from 'marked';
 
 import './styles.css';
 
@@ -13,6 +15,7 @@ function AddTopic(){
     const [subject, setSubject] = useState('');
     const [course, setCourse] = useState('');
     const [content, setContent] = useState('');
+    const [preview, setPreview] = useState(false);
 
     const handleFormSubmit = () => {
         const data = {
@@ -22,8 +25,6 @@ function AddTopic(){
             course,
             content
         }
-
-        console.log(data);
 
         api.post('topics', data).then((response) => {
             if(response.status === 201) alert('Success!');
@@ -37,33 +38,41 @@ function AddTopic(){
             <Header />
             <SideBar active="plus"/>
             <div className="form">
-                <div className="input-group">
-                    <label htmlFor="title">Título:</label>
-                    <input type="text" id="title" onChange={(e) => setTitle(e.target.value)}/>
-                </div>
-                <div className="input-group">
-                    <label htmlFor="author">Autor:</label>
-                    <input type="text" id="author" onChange={(e) => setAuthor(e.target.value)}/>
-                </div>
-                <div className="input-group">
-                    <label htmlFor="course">Curso:</label>
-                    <select value={course} onChange={(e) => setCourse(e.target.value)}id="course">
-                        <option value="" disabled hidden>Selecione uma opção</option>
-                        <option value="1MEC">1º Mecânica</option>
-                        <option value="2MEC">2º Mecânica</option>
-                        <option value="3MEC">3º Mecânica</option>
-                        <option value="1INF">1º Informática</option>
-                    </select>
-                </div>
-                <div className="input-group">
-                    <label htmlFor="subject">Matéria:</label>
-                    <input type="text" id="subject" onChange={(e) => setSubject(e.target.value)}/>
-                </div>
-                <div className="input-group">
-                    <label htmlFor="text">Conteúdo:</label>
-                    <textarea id="text" onChange={(e) => setContent(e.target.value)}></textarea>
-                </div>
-                <button onClick={handleFormSubmit}>Criar</button>
+                <button className={preview ? 'pv-active pv-btn' : 'pv-btn'} onClick={() => setPreview(!preview)}>Preview: {preview ? 'Ativado' : 'Desativado'}</button>
+                {preview ? 
+                    (
+                        <div className="pv-box" dangerouslySetInnerHTML={{ __html: marked(content, {breaks: true})}}></div>
+                    ) : (
+                        <div>
+                            <div className="input-group">
+                                <label htmlFor="title">Título:</label>
+                                <input value={title} type="text" id="title" onChange={(e) => setTitle(e.target.value)}/>
+                            </div>
+                            <div className="input-group">
+                                <label htmlFor="author">Autor:</label>
+                                <input value={author} type="text" id="author" onChange={(e) => setAuthor(e.target.value)}/>
+                            </div>
+                            <div className="input-group">
+                                <label htmlFor="course">Curso:</label>
+                                <select value={course} onChange={(e) => setCourse(e.target.value)}id="course">
+                                    <option value="" disabled hidden>Selecione uma opção</option>
+                                    <option value="1MEC">1º Mecânica</option>
+                                    <option value="2MEC">2º Mecânica</option>
+                                    <option value="3MEC">3º Mecânica</option>
+                                    <option value="1INF">1º Informática</option>
+                                </select>
+                            </div>
+                            <div className="input-group">
+                                <label htmlFor="subject">Matéria:</label>
+                                <input value={subject} type="text" id="subject" onChange={(e) => setSubject(e.target.value)}/>
+                            </div>
+                            <div className="input-group">
+                                <label htmlFor="text">Conteúdo:</label>
+                                <textarea value={content} id="text" onChange={(e) => setContent(e.target.value)}></textarea>
+                            </div>
+                            <button className="add-btn" onClick={handleFormSubmit}>Criar</button> 
+                        </div>
+                )}
             </div>
         </>
     )
