22a1ec615a89d7adf743bb191374cb756ac44ebd
Eduardo Correia <eduardo.correia.ehsc@gmail.com>
2021-02-20 17:44:16 -0300

    Fim da estilização da página de listagem de monitorias

--- desktop/src/pages/Clipboard/index.jsx
+++ desktop/src/pages/Clipboard/index.jsx
@@ -9,41 +9,16 @@ import ClassItem from '../../components/ClassItem';
 import './styles.css';
 
 import filterIcon from '../../assets/icons/filter.svg';
+import reloadIcon from '../../assets/icons/reload.svg';
 
 function Clipboard(){
     const [data, setData] = useState([]);
-    const [filteredData, setFilteredData] = useState([]);
-    const [filter, setFilter] = useState('');
 
     useEffect(() => {
         api.get('notify').then(res => {
             setData(res.data);
-            setFilteredData(res.data);
         });
     }, []);
-
-    useEffect(() => {
-        let filteredDataArray;
-
-        if(filter === '') {
-            filteredDataArray = data;
-        }else{
-            filteredDataArray = data.filter(item => {
-                if(item.subject === filter) return item;
-            })
-        }
-
-        setFilteredData(filteredDataArray);
-    }, [filter]);
-
-    function handleResetFilter(){
-        setFilter('');
-        const inputs = document.querySelectorAll('input');
-
-        inputs.forEach(input => {
-            input.checked = false;
-        })
-    }
 
     return (
         <>
@@ -55,81 +30,87 @@ function Clipboard(){
                     <div className="options">
                         <div className="filter-group">
                             <span>Nome:</span>
-                            <input type="text"/>
+                            <input type="text" placeholder="Pesquise um nome aqui..."/>
                         </div>
                         <div className="filter-group">
                             <span>Curso:</span>
                             <div className="checkbox-group">
-                                <input type="checkbox"/>
-                                <label htmlFor="">Enfermagem</label>
+                                <input type="checkbox" id="enf" name="course"/>
+                                <label htmlFor="enf">Enfermagem</label>
                             </div>
                             <div className="checkbox-group">
-                                <input type="checkbox"/>
-                                <label htmlFor="">Edificações</label>
+                                <input type="checkbox" id="edf" name="course"/>
+                                <label htmlFor="edf">Edificações</label>
                             </div>
                             <div className="checkbox-group">
-                                <input type="checkbox"/>
-                                <label htmlFor="">Geodésia</label>
+                                <input type="checkbox" id="geo" name="course"/>
+                                <label htmlFor="geo">Geodésia</label>
                             </div>
                             <div className="checkbox-group">
-                                <input type="checkbox"/>
-                                <label htmlFor="">Informática</label>
+                                <input type="checkbox" id="inf" name="course"/>
+                                <label htmlFor="inf">Informática</label>
                             </div>
                             <div className="checkbox-group">
-                                <input type="checkbox"/>
-                                <label htmlFor="">Mecânica</label>
+                                <input type="checkbox" id="mec" name="course"/>
+                                <label htmlFor="mec">Mecânica</label>
                             </div>
                             <div className="checkbox-group">
-                                <input type="checkbox"/>
-                                <label htmlFor="">Qualidade</label>
+                                <input type="checkbox" id="qld" name="course"/>
+                                <label htmlFor="qld">Qualidade</label>
                             </div>
                             <div className="checkbox-group">
-                                <input type="checkbox"/>
-                                <label htmlFor="">Física e Matemática</label>
+                                <input type="checkbox" id="fem" name="course"/>
+                                <label htmlFor="fem">Física e Matemática</label>
                             </div>
                             <div className="checkbox-group">
-                                <input type="checkbox"/>
-                                <label htmlFor="">Humanas</label>
+                                <input type="checkbox" id="hum" name="course"/>
+                                <label htmlFor="hum">Humanas</label>
                             </div>
                             <div className="checkbox-group">
-                                <input type="checkbox"/>
-                                <label htmlFor="">Biologia e Química</label>
+                                <input type="checkbox" id="beq" name="course"/>
+                                <label htmlFor="beq">Biologia e Química</label>
                             </div>
                         </div>
                         <div className="filter-group">
                             <span>Série:</span>
                             <div className="checkbox-group">
-                                <input type="checkbox"/>
-                                <label htmlFor="">1º ano</label>
+                                <input type="checkbox" id="year1" name="year"/>
+                                <label htmlFor="year1">1º ano</label>
                             </div>
                             <div className="checkbox-group">
-                                <input type="checkbox"/>
-                                <label htmlFor="">2º ano</label>
+                                <input type="checkbox"id="year2" name="year"/>
+                                <label htmlFor="year2">2º ano</label>
                             </div>
                             <div className="checkbox-group">
-                                <input type="checkbox"/>
-                                <label htmlFor="">3º ano</label>
+                                <input type="checkbox"id="year3" name="year"/>
+                                <label htmlFor="year3">3º ano</label>
                             </div>
                         </div>
                         <div className="filter-group">
                             <span>Presente:</span>
                             <div className="checkbox-group">
-                                <input type="radio"/>
-                                <label htmlFor="">Sim</label>
+                                <input type="radio" id="yes" name="presence"/>
+                                <label htmlFor="yes">Sim</label>
                             </div>
                             <div className="checkbox-group">
-                                <input type="radio"/>
-                                <label htmlFor="">Não</label>
+                                <input type="radio" id="no" name="presence"/>
+                                <label htmlFor="no">Não</label>
                             </div>
                         </div>
                     </div>
                     <div className="filter-btns">
-                        <button>Reset</button>
-                        <button>Filtrar</button>
+                        <button>
+                            <img src={reloadIcon} alt="Resetar"/>
+                            <span>Reset</span>
+                        </button>
+                        <button>
+                            <img src={filterIcon} alt="Filter"/>
+                            <span>Filter</span>
+                        </button>
                     </div>
                 </div>
                 <div className="results">
-                    {filteredData.map(({topic, year, subject, ra, phone, name, email}, index) => (
+                    {data.map(({topic, year, subject, ra, phone, name, email}, index) => (
                         <ClassItem 
                             key={index}
                             topic={topic}
