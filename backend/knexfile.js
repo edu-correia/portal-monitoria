8c8775a79ae780396c2a990c83b7acb1d61f7e0a
Eduardo Correia <eduardo.correia.ehsc@gmail.com>
2021-02-23 17:52:07 -0300

    Instalação de novas dependências e alteração na conexão

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -8,7 +8,8 @@ module.exports = {
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE
+      database: process.env.DB_DATABASE,
+      port: process.env.DB_PORT
     },
     migrations: {
       tableName: 'knex_migrations',
